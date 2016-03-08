a116777d5816eeea0dabab019809e546fbf14db7
unknown <muneerahmed48@yahoo.com>
2016-03-08 20:37:51 +0530

    msg

--- register/index.php
+++ register/index.php
@@ -1,16 +1,17 @@
 <?php
 	require "../init.php";
 	
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$password = $_POST["password"];
-	$mobile = $_POST["mobile"];
+	$Email = $_GET["Email"];
+	$Mobile = $_GET["Mobile"];
+	$AccountNumber = $_GET["AccountNumber"];
+	$Name = $_GET["Name"];
+	$Password = $_GET["Password"];
 	
-	$sql = "INSERT INTO Users(Name, Email, Mobile, Password) VALUES('$name', '$email', '$mobile', '$password');";
+	$sql = "INSERT INTO Users (Email, Mobile, AccountNumber, Name, Password) VALUES('$Email', '$Mobile', '$AccountNumber', '$Name', '$Password');";
 	
 	if(mysqli_query($conn, $sql))
 	{
-		echo "Registration Successfull";
+		echo "Registration Successfull!";
 	}
 	else
 	{
