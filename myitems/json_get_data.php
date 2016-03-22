a1407115488fcfd3a18e350f0d367a2d087dcdbe
unknown <muneerahmed48@yahoo.com>
2016-03-22 16:31:20 +0530

    msg

---
+++ myitems/json_get_data.php
@@ -0,0 +1,20 @@
+ <?php
+	require "init.php";
+	
+	$Email = $_GET["Email"];
+	
+	$sql = "SELECT * FROM Purchases WHERE Buyer = '$Email'";
+	
+	$result = mysqli_query($conn, $sql);
+	
+	$response = array();
+	
+	while($row = mysqli_fetch_array($result))
+	{
+		array_push($response, array("ItemName"=>$row[2], "ItemPrice"=>$row[3], "ItemCategory"=>$row[4]));
+	}
+	
+	echo json_encode(array("server_response"=>$response));
+	
+	mysqli_close($conn);
+?>
