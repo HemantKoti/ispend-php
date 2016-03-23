674b051f021f4bb0d99bac387a3a18cb08322661
unknown <muneerahmed48@yahoo.com>
2016-03-23 23:02:58 +0530

    msg

--- piechart/get_budget.php
+++ piechart/get_budget.php
@@ -3,11 +3,23 @@
 	
 	$Email = $_GET["Email"];
 	
-	$sql = "SELECT `ItemCategory`, SUM(`ItemPrice`) FROM `Purchases` WHERE `Buyer` = '$Email' GROUP BY `ItemCategory`;";
 	
-	$result = mysqli_query($conn, $sql);
+		$budget["Entertainment"] = $row[1];
+		$budget["Electronics"] = $row[0];
+		$budget["Fashion"] = $row[2];
+		$budget["Other"] = $row[4];
 	
 	$budget = array();
+	
+	$sql1 = "SELECT SUM(`ItemPrice`) FROM `Purchases` WHERE `Buyer` = '$Email' AND `ItemCategory` = 'Food';";
+	$result = mysqli_query($conn, $sql);
+	if(mysqli_num_rows($result) > 0)
+	{
+		$row = mysqli_fetch_assoc($result);
+		$budget["Food"] = $row[0];
+	}
+	
+	
 	
 	if(mysqli_num_rows($result) > 0)
 	{
