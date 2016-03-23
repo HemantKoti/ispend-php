f7b9e77e90b6a30c63d9637a3300f3b51ea1cbfc
unknown <muneerahmed48@yahoo.com>
2016-03-23 11:13:50 +0530

    msg

--- register/index.php
+++ register/index.php
@@ -12,6 +12,21 @@
 	if(mysqli_query($conn, $sql))
 	{
 		echo "Registration Successfull";
+		
+		$sql1 = INSERT INTO `ispend`.`Purchases` (`PurchaseID`, `Buyer`, `ItemName`, `ItemPrice`, `ItemCategory`) VALUES (NULL, '$Email', NULL, '0', 'Electronics');
+		mysqli_query($conn, $sql1);
+		
+		$sql2 = INSERT INTO `ispend`.`Purchases` (`PurchaseID`, `Buyer`, `ItemName`, `ItemPrice`, `ItemCategory`) VALUES (NULL, '$Email', NULL, '0', 'Entertainment');
+		mysqli_query($conn, $sql1);
+		
+		$sql3 = INSERT INTO `ispend`.`Purchases` (`PurchaseID`, `Buyer`, `ItemName`, `ItemPrice`, `ItemCategory`) VALUES (NULL, '$Email', NULL, '0', 'Fashion');
+		mysqli_query($conn, $sql1);
+		
+		$sql4 = INSERT INTO `ispend`.`Purchases` (`PurchaseID`, `Buyer`, `ItemName`, `ItemPrice`, `ItemCategory`) VALUES (NULL, '$Email', NULL, '0', 'Food');
+		mysqli_query($conn, $sql1);
+		
+		$sql5 = INSERT INTO `ispend`.`Purchases` (`PurchaseID`, `Buyer`, `ItemName`, `ItemPrice`, `ItemCategory`) VALUES (NULL, '$Email', NULL, '0', 'Other');
+		mysqli_query($conn, $sql1);
 	}
 	else
 	{
