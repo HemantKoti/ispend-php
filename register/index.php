e3a518d9e5308f27199c99539db31977adc45fd5
unknown <muneerahmed48@yahoo.com>
2016-03-23 12:01:06 +0530

    msg

--- register/index.php
+++ register/index.php
@@ -3,11 +3,10 @@
 	
 	$Email = $_POST["Email"];
 	$Mobile = $_POST["Mobile"];
-	$AccountNumber = $_POST["AccountNumber"];
 	$Name = $_POST["Name"];
 	$Password = $_POST["Password"];
 	
-	$sql = "INSERT INTO Users(Email, Mobile, AccountNumber, Name, Password) VALUES('$Email', '$Mobile', '$AccountNumber', '$Name', '$Password');";
+	$sql = "INSERT INTO Users(Email, Mobile, Name, Password) VALUES('$Email', '$Mobile', '$Name', '$Password');";
 	
 	if(mysqli_query($conn, $sql))
 	{
