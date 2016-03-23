816ddbe56a784375da2010098d5dfc06f1267dc4
unknown <muneerahmed48@yahoo.com>
2016-03-23 23:06:19 +0530

    msg

--- piechart/get_budget.php
+++ piechart/get_budget.php
@@ -3,33 +3,14 @@
 	
 	$Email = $_GET["Email"];
 	
-	
-		$budget["Entertainment"] = $row[1];
-		$budget["Electronics"] = $row[0];
-		$budget["Fashion"] = $row[2];
-		$budget["Other"] = $row[4];
-	
 	$budget = array();
 	
-	$sql1 = "SELECT SUM(`ItemPrice`) FROM `Purchases` WHERE `Buyer` = '$Email' AND `ItemCategory` = 'Food';";
+	$sql1 = "SELECT SUM(`ItemPrice`) AS Food FROM `Purchases` WHERE `Buyer` = '$Email' AND `ItemCategory` = 'Food';";
 	$result = mysqli_query($conn, $sql);
 	if(mysqli_num_rows($result) > 0)
 	{
 		$row = mysqli_fetch_assoc($result);
-		$budget["Food"] = $row[0];
-	}
-	
-	
-	
-	if(mysqli_num_rows($result) > 0)
-	{
-		$row = mysqli_fetch_assoc($result);
-		
-		echo $result;
-	}
-	else
-	{
-		//echo "The email and password you entered don't match.";
+		$budget["Food"] = $row["Food"];
 	}
 	
 	echo json_encode($budget);
