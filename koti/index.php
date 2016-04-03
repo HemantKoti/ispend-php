75ad469174061dfc3ff34c1234b431b552092cbb
Hemant Koti <Hemant Koti>
2016-04-03 20:59:14 +0530

    koti sendemail

--- koti/index.php
+++ koti/index.php
@@ -1,20 +1,30 @@
-<html>
-    <body>
-        <?php
-        $email = "kotihemant@gmail.com";
-        $body = "Hello are nainaaaaaaaaaaaaaa";
-         
-		$headers = "From: kotihemant@gmail.com \r\n";
-		$headers .= "Reply-To: kotihemant@gmail.com \r\n";
-		$headers .= "Return-Path: kotihemant@gmail.com\r\n";
-		$headers .= "X-Mailer: PHP \r\n";
-	
-        $retval = mail($email, "Ispend Email", $body, $headers);
-        if ($retval == true) {
-            echo nl2br("Email Sent Successfully to : koti \n");
-        } else {
-            echo nl2br("Email Not Sent\n");
-        }
-        ?>
-    </body>
-</html>
+<?php
+require_once "Mail.php";
+
+$from = '<kotihemant@gmail.com>';
+$to = '<kotihemant5@gmail.com>';
+$subject = 'Hi!';
+$body = "Hi,\n\nHow are you?";
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'johndoe@gmail.com',
+        'password' => 'passwordxxx'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+}
+?>
