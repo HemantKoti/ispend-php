75f20cb447d9298eaaaf04bba9ce726d0059fdd5
Hemant Koti <Hemant Koti>
2016-04-03 22:40:21 +0530

    koti sendemail

--- koti/index.php
+++ koti/index.php
@@ -1,24 +1,52 @@
-<?php
-require_once('class.phpmailer.php');
-
-$mail = new PHPMailer(); // create a new object
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
-$mail->IsHTML(true);
-$mail->Username = "kotihemant5@gmail.com";
-$mail->Password = "hempad95";
-$mail->SetFrom("kotihemant5@gmail.com");
-$mail->Subject = "Test";
-$mail->Body = "hello";
-$mail->AddAddress("kotihemant@gmail.com");
-
- if(!$mail->Send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
- } else {
-    echo "Message has been sent";
- }
- ?>
+<html>
+    <body>
+        <?php
+        require "../init.php";
+        $pwd = $_POST['psd'];
+        $sqlUsers = "SELECT Email FROM Users;";
+        $email = "";
+        $offers = "";
+        $purchase = "";
+        $response = array();
+        $resultUsers = mysqli_query($conn, $sqlUsers);
+        if (!isset($_POST['submit'])) {
+            echo nl2br("Click Submit first\n");
+        }
+        if (mysqli_num_rows($resultUsers) > 0) {
+            while ($rowUsers = mysqli_fetch_assoc($resultUsers)) {
+                if (filter_var($rowUsers["Email"], FILTER_VALIDATE_EMAIL)) {
+                    $email = $rowUsers["Email"];
+                    $sqlPurchase = "SELECT MAX(ItemPrice) AS HighestPrice, ItemCategory FROM Purchases WHERE Buyer = '$email';";
+                    $resultPurchase = mysqli_query($conn, $sqlPurchase);
+                    if (mysqli_num_rows($resultPurchase) > 0) {
+                        $rowPurchase = mysqli_fetch_assoc($resultPurchase);
+                        if ($rowPurchase["HighestPrice"] != 0) {							
+                            $purchase = $rowPurchase["ItemCategory"];
+                            $sqlOffers = "SELECT Offer FROM Offers WHERE Category = '$purchase';";
+                            $resultOffers = mysqli_query($conn, $sqlOffers);
+                            $body = "";
+                            if (mysqli_num_rows($resultOffers) > 0) {
+                                while ($rowOffers = mysqli_fetch_assoc($resultOffers)) {
+                                    $body .= $rowOffers["Offer"] . "\n";
+                                }
+                                array_push($response, array("Email" => $rowUsers["Email"], "Body" => $body));
+                            } else {
+                                echo nl2br("No results in Offers Table\n");
+                            }
+                        } else {
+                            echo nl2br("Purchase Anything other than 0\n");
+                        }
+                    } else {
+                        echo nl2br("No results in Purchase Table\n");
+                    }
+                } else {
+                    echo nl2br("Enter a Valid Email\n");
+                }
+            }
+        } else {
+            echo nl2br("No results in Users Table\n");
+        }
+        echo json_encode(array("server_response" => $response));
+        ?>
+    </body>
+</html>
