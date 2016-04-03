322b370b93accd94f493f0b11dca3dcc042f10d7
Hemant Koti <Hemant Koti>
2016-04-03 18:57:06 +0530

    koti sendemail

--- koti/send_email.php
+++ koti/send_email.php
@@ -1,6 +1,42 @@
- <?php
-	require "../init.php";
-	
-	//ikda nee php code anta
-	//database conn ki direct $conn object use cheseyi
+<?php
+        require "../init.php";
+		$sqlUsers = "SELECT Email FROM Users;";
+        $email = "";
+        $offers = "";
+        $purchase = "";
+        $resultUsers = mysqli_query($conn, $sqlUsers);
+        if (mysqli_num_rows($resultUsers) > 0) {
+            while ($rowUsers = mysqli_fetch_assoc($resultUsers)) {
+                if (filter_var($rowUsers["Email"], FILTER_VALIDATE_EMAIL)) {
+                    $email = $rowUsers["Email"];
+                    $sqlPurchase = "SELECT MAX(ItemPrice) AS HighestPrice, ItemCategory FROM Purchases WHERE Buyer = '$email';";
+                    $resultPurchase = mysqli_query($conn, $sqlPurchase);
+                    if (mysqli_num_rows($resultPurchase) > 0) {
+                        $rowPurchase = mysqli_fetch_assoc($resultPurchase);
+                        if ($rowPurchase["HighestPrice"] != 0) {
+                            $purchase = $rowPurchase["ItemCategory"];
+                            $sqlOffers = "SELECT Offer FROM Offers WHERE Category = '$purchase';";
+                            $resultOffers = mysqli_query($conn, $sqlOffers);
+                            $body = "";
+                            if (mysqli_num_rows($resultOffers) > 0) {
+                                while ($rowOffers = mysqli_fetch_assoc($resultOffers)) {
+                                    $body .= $rowOffers["Offer"] . "\n";
+                                }
+                                mail($rowUsers["Email"], "Exciting Offers for you", $body);
+                            } else {
+                                echo "No results in Offers Table";
+                            }
+                        } else {
+                            echo "Purchase Something";
+                        }
+                    } else {
+                        echo "No results in Purchase Table";
+                    }
+                } else {
+                    echo "Enter a Valid Email";
+                }
+            }
+        } else {
+            echo "No results in Users Table";
+        }
 ?>
