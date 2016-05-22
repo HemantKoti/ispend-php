c4a34df4d821bf6874a87e6a1b81c7410fe6e2d5
Muneer Ahmed <muneerahmed94@gmail.com>
2016-05-22 13:54:49 +0530

    learning json

--- offline/json.php
+++ offline/json.php
@@ -1,12 +1,14 @@
 <?php
-	$json = array(
-		"name" => "muneer",
-		"name" => "mujeeb",
-		"name" => "noor",
-		"name" => "nazeem",
-		"name" => "mohsin",
-		"name" => "sameena"
-	);
+	$post_data = array(
+  'item' => array(
+    'item_type_id' => $item_type,
+    'string_key' => $string_key,
+    'string_value' => $string_value,
+    'string_extra' => $string_extra,
+    'is_public' => $public,
+   'is_public_for_contacts' => $public_contacts
+  )
+);
 	
-	echo json_encode($json);
+	echo json_encode($post_data);
 ?>
