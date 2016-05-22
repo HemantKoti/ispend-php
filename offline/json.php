43fdeb8bc4e1bfed89135836cb4bffeb18c18958
Muneer Ahmed <muneerahmed94@gmail.com>
2016-05-22 13:57:59 +0530

    learning json

--- offline/json.php
+++ offline/json.php
@@ -1,12 +1,9 @@
 <?php
 	$post_data = array(
-  'item' => array(
-    'item_type_id' => $item_type,
-    'string_key' => $string_key,
-    'string_value' => $string_value,
-    'string_extra' => $string_extra,
-    'is_public' => $public,
-   'is_public_for_contacts' => $public_contacts
+  'purchase' => array(
+    'item_name' => "Moto G",
+    'item_category' => "Electronics",
+    'item_price' => "14000"
   )
 );
 	
