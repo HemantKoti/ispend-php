e99388ded189bc804c4804b29d14f04c6d616991
Muneer Ahmed <muneerahmed94@gmail.com>
2016-05-22 13:59:57 +0530

    learning json

--- offline/json.php
+++ offline/json.php
@@ -1,10 +1,27 @@
 <?php
 	$post_data = array(
-  'purchase' => array(
-    'item_name' => "Moto G",
-    'item_category' => "Electronics",
-    'item_price' => "14000"
-  )
+	  'purchase' => array(
+		array(
+			'item_name' => "Moto G",
+			'item_category' => "Electronics",
+			'item_price' => "14000"
+		),
+		array(
+			'item_name' => "Moto G",
+			'item_category' => "Electronics",
+			'item_price' => "14000"
+		),
+		array(
+			'item_name' => "Moto G",
+			'item_category' => "Electronics",
+			'item_price' => "14000"
+		),
+		array(
+			'item_name' => "Moto G",
+			'item_category' => "Electronics",
+			'item_price' => "14000"
+		)
+	  )
 );
 	
 	echo json_encode($post_data);
