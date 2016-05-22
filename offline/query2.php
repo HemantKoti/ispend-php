2df368910f3a0cd831b909e59533b3291c703d15
Muneer Ahmed <muneerahmed94@gmail.com>
2016-05-22 21:21:09 +0530

    json practice

--- offline/query2.php
+++ offline/query2.php
@@ -9,8 +9,11 @@
 		$fid = $record['fish_id'];
 		$flat = $record['fish_lat'];
 		$flon = $record['fish_lon'];
-
-		if(mysqli_query($mysqli,"INSERT INTO  `ispend`.`fishes` (`fish_lat` ,`fish_lon`) VALUES ('$flat',  '$flon')")) {
+		
+		$sql1 = "INSERT INTO  `ispend`.`fishes` (`fish_type_id` ,`fish_lat` ,`fish_lon`) VALUES (NULL,  '$flat',  '$flon');";
+		mysqli_query($conn, $sql1);
+		
+		if(mysqli_query($mysqli,"")) {
 			echo "yes";
 		}
 		else {
