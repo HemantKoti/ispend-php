b975aadcf525d9bcf940595eb4cf3e4a7844aa35
Muneer Ahmed <muneerahmed94@gmail.com>
2016-05-30 21:14:09 +0530

    msg

--- final/s2d/transactions.php
+++ final/s2d/transactions.php
@@ -1,20 +1,16 @@
  <?php
-	require "../../../init.php";
+	require "../../init.php";
 	
 	$Email = $_POST["Email"];
-	$UploaderMAC = $_POST["UploaderMAC"];
-	$UploaderMAC = $_POST["UploaderMAC"];
-	$LastDownloadTime = $_POST["LastDownloadTime"];
 		
-	$sql = "SELECT * FROM `ispend`.`Purchases` WHERE `Buyer` = '$Email' AND `UploadedTime` > '$LastDownloadTime' AND `UploaderMAC` NOT LIKE '$UploaderMAC';";
+	$sql = "SELECT * FROM `ispend`.`FTransactions` WHERE `Email` = '$Email';";
 	
 	$result = mysqli_query($conn, $sql);
 	
 	$response = array();
 	
-	while($row = mysqli_fetch_array($result))
-	{
-		array_push($response, array("ItemName"=>$row[2], "ItemPrice"=>$row[3], "ItemCategory"=>$row[4], "PurchaseTime"=>$row[5]));
+	while($row = mysqli_fetch_array($result)) {
+		array_push($response, array("TransactionType"=>$row[2], "TransactionDate"=>$row[3], "TransactionCategory"=>$row[4], "TransactionAmount"=>$row[5], "TransactionDescription"=>$row[6]));
 	}
 	
 	echo json_encode(array("server_response"=>$response));
